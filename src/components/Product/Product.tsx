9d6c2422769398aba7c0c00bbc9c2b508a5624f1
xdothinh <xdodinh.thinh@contract.isid.co.jp>
2022-12-23 22:37:27 +0700

    fix: create components

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Product: React.FC = () => {
-  return <div className="text-center text-xl text-blue-300">xin chao</div>;
+  return (
+    <div className="text-center text-xl text-blue-300">
+      xin chao đỗ đình thinh
+    </div>
+  );
 };
 
 export default Product;
