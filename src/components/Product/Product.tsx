ccda25403845b2f8d81c9d51f913d9dbde1a0337
pheo1712 <dinthinh1712@gmail.com>
2022-12-23 22:46:41 +0700

    fix: test

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,6 +4,7 @@ const Product: React.FC = () => {
   return (
     <div className="text-center text-xl text-blue-300">
       xin chao đỗ đình thinh
+      <p className="text-2xl">hihihihihihihihihihihi</p>
     </div>
   );
 };
