10169d022cfcd202df0a92f1c0cc22861b01cad4
xdothinh <xdodinh.thinh@contract.isid.co.jp>
2022-12-23 21:55:16 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/helper/Layout";
+import React from "react";
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+};
+
+export default IndexPage;
